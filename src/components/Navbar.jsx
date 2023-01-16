eb8644678cf5cede7e1f7867bbd31cf8b04bc4a2
Lindrit <lindritsulaj2.0@gmail.com>
2023-01-16 20:44:21 +0100

    Responsive Navbar Layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import { useTheme } from '../ThemeContext';
 import useMediaQuery from '../assets/useMediaQuery';
 
+const barsVariant = {
+  animate: (direction) => {
+    if (direction === "bottom") {
+      return { rotate: 50, y: 5 }
+    } else {
+      return { rotate: 130, y: -7  }
+    }
+  },
+  exit: {
+    y: 0,
+    rotate: 0,
+  }
+}
+
+const slider = {
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: { type: 'just' }
+  },
+  exit: {
+    x: "-100vw",
+    opacity: 0,
+  }
+}
+
 const Navbar = () => {
+  const [windowWidth, breakpoint] = useMediaQuery();
+  const [navOpened, setNavOpened] = useState(false);
+
   return (
     <div className='bg-white fixed top-0 w-full'>
       <nav className='container h-[75px] flex justify-between items-center mx-auto px-2'>
@@ -16,17 +45,32 @@ const Navbar = () => {
 
 
         </h1>
-        <ul className='flex gap-4'>
+        <ul className='hidden md:flex gap-4'>
           <Link label="Home" />
           <Link label="Blog" />
           <Link label="Services" />
           <Link label="Reviews" />
         </ul>
 
-        <div className='flex gap-2'>
+        <div className='hidden md:flex gap-2'>
           <motion.button className='px-2 text-neutral-900'>Log in</motion.button>
           <motion.button className='bg-blue-custom rounded-sm px-3 py-2 text-white'>Sign Up</motion.button>
         </div>
+
+        <motion.div className='md:hidden' animate={navOpened ? 'animate' : 'exit'} >
+          <button className='flex flex-col items-center gap-1' onClick={() => setNavOpened(!navOpened)}>
+            <motion.div custom="bottom" variants={barsVariant} className="w-6 bg-neutral-900 h-[2px] rounded-full"></motion.div>
+            <motion.div variants={{ exit: { opacity: 1, }, animate: { opacity: 0 } }} className="w-6 bg-neutral-900 h-[2px] rounded-full"></motion.div>
+            <motion.div custom="top" variants={barsVariant} className="w-6 bg-neutral-900 h-[2px] rounded-full"></motion.div>
+          </button>
+
+          <motion.div variants={slider} className='absolute h-[calc(100vh-70px)] left-0 bg-white top-[70px]'>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem consectetur dicta provident consequuntur, cumque fugit molestiae fugiat quibusdam tempore totam quod adipisci, corrupti atque est nulla. Inventore ipsam atque quae.
+            </p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti sequi doloremque magni, sit dolorum nemo quidem ducimus quasi nesciunt? Perspiciatis aliquam libero nesciunt autem voluptas labore minima nihil. Vel, repellendus?</p>
+          </motion.div>
+        </motion.div>
       </nav>
     </div>
   )
